0d9082bdb0e24f3efbfe29cc3eb9c1dc65a83fd0
Shaon1342 <mahmood_shaon@yahoo.com>
2018-01-10 16:30:35 +0600

    further modified with bootstrap

--- blog/forms.py
+++ blog/forms.py
@@ -8,15 +8,15 @@ class PostForm(forms.ModelForm):
         model = Post
         fields = ('author','title','slug','status','text','tags',)
 
-    def clean_slug(self):
-        slug = self.cleaned_data.get('slug', '')
-        existing = Post.objects.filter(slug=slug)
-        if len(existing):
-            raise forms.ValidationError(
-                "Slug already exits",
-                code='invalid'
-            )
-        return slug
+    # def clean_slug(self):
+    #     slug = self.cleaned_data.get('slug', '')
+    #     existing = Post.objects.filter(slug=slug)
+    #     if len(existing):
+    #         raise forms.ValidationError(
+    #             "Slug already exits",
+    #             code='invalid'
+    #         )
+    #     return slug
 
 
 class LoginForm(forms.Form):
